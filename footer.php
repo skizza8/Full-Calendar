b83db0fe868a776a2881d8778f8da8bf406f07df
Samuel.Kizza <skizza8@gmail.com>
2021-03-20 13:47:10 +0300

    second commit

--- footer.php
+++ footer.php
@@ -8,17 +8,17 @@
 </div>
 <!-- main wrapper start -->
 <!-- jquery latest version -->
-<script src="<?php echo base_url('srt_assets/js/vendor/jquery-2.2.4.min.js') ?>"></script>
+<script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
 <!-- bootstrap 4 js -->
-<script src="<?php echo base_url('srt_assets/js/popper.min.js') ?>"></script>
-<script src="<?php echo base_url('srt_assets/js/bootstrap.min.js') ?>"></script>
-<script src="<?php echo base_url('srt_assets/js/owl.carousel.min.js') ?>"></script>
-<script src="<?php echo base_url('srt_assets/js/metisMenu.min.js') ?>"></script>
-<script src="<?php echo base_url('srt_assets/js/jquery.slimscroll.min.js') ?>"></script>
-<script src="<?php echo base_url('srt_assets/js/jquery.slicknav.min.js') ?>"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="assets/js/jquery.slicknav.min.js"></script>
 
 <!-- Breeding Calendar -->
-<script src="<?php echo base_url('gestation_calendar/gestation_calendar.js') ?>"></script>
+<script src="assets/js/gestation_calendar.js"></script>
 
 </body>
 
